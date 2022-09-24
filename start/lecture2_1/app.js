63f06203c7e78871caa97e11c9edd2d8f43f6503
Zhminko Roman <92923411+ZhminkoRoman@users.noreply.github.com>
2022-09-24 17:51:31 +0300

    feat(THREE.js lessons): add three varuables

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -5,6 +5,14 @@ class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
+
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+		const renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+
+		container.appendChild( renderer.domElement );
     
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
