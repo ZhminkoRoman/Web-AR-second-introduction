8c7a68ab523e6a112f427b2f77516e55fa151b59
Zhminko Roman <92923411+ZhminkoRoman@users.noreply.github.com>
2023-06-20 23:39:24 +0300

    feat: re-start learning THREE.js and WebXR

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -1,43 +1,17 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import * as THREE from "../../libs/three/three.module.js";
+import { OrbitControls } from "../../libs/three/jsm/OrbitControls.js";
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
+class App {
+  constructor() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
 
-		this.scene = new THREE.Scene();
-		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    window.addEventListener("resize", this.resize.bind(this));
+  }
 
-		this.renderer = new THREE.WebGLRenderer();
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
+  resize() {}
 
-		container.appendChild( this.renderer.domElement );
-
-		const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-		this.cube = new THREE.Mesh( geometry, material );
-
-		this.scene.add( this.cube );
-
-		this.camera.position.z = 5;
-
-		this.renderer.setAnimationLoop( this.render.bind(this) )
-    
-        window.addEventListener('resize', this.resize.bind(this) );
-	}
-
-	animate() {
-
-	}
-    
-    resize(){
-        
-    }
-    
-	render( ) {   
-		this.renderer.render( this.scene, this.camera );
-    }
+  render() {}
 }
 
 export { App };
